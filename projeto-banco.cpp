a2d2cab016cca16749f70b7131fc7655c92ec426
Iallan Setenta <iallanguimel@hotmail.com>
2023-09-12 16:39:29 -0300

    att

---
+++ projeto-banco.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct ContaCliente
+{
+    string nome;
+    string cpf;
+    string endereco;
+    string tel;
+    string email;
+    int agencia;
+    int conta;
+};
+
+
+
+int main(){
+
+    return 0;
+} 
